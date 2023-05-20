90974a619e3720ea5b6fdc6dd6cce108d10e85c3
Zeth <zeth@bmisw.com>
2023-05-19 20:01:32 -0600

    added Bruce Link

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -1,46 +1,46 @@
-import React from 'react';
-import './footer.css'; 
-import { FaFacebookF, FaInstagram, FaMapMarkerAlt } from "react-icons/fa"
+import React from "react";
+import "./footer.css";
+import { FaFacebookF, FaInstagram, FaMapMarkerAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-
 const footer = () => {
-    const fbookClick = () =>{
-        window.open("#");
-    };
-    const instaClick = () =>{
-        window.open("#");
-    };
-    const mapClick = () =>{
-        window.open("https://goo.gl/maps/FSqTYpgAYW1RZ65x5");
-    };
+  const fbookClick = () => {
+    window.open("#");
+  };
+  const instaClick = () => {
+    window.open("#");
+  };
+  const mapClick = () => {
+    window.open("https://goo.gl/maps/FSqTYpgAYW1RZ65x5");
+  };
 
-    return(
-        <div className="footer">
-            <div className="navDiv">
-            <Link className='nav' to={'/info'}>
-                More info
-            </Link>
-            |
-            <Link className='nav' to={'/OUR'}>
-                Operation Undrground Railroad
-            </Link>
-            |
-            <Link className='nav' to={'/'}>
-                 Home 
-            </Link>
-            </div>
-            
-            {/* <FaFacebookF onClick={fbookClick} className="icons"/>
+  return (
+    <div className="footer">
+      <div className="navDiv">
+        <Link className="nav" to={"/info"}>
+          More info
+        </Link>
+        |
+        <Link className="nav" to={"/OUR"}>
+          Operation Undrground Railroad
+        </Link>
+        |
+        <Link className="nav" to={"/"}>
+          Home
+        </Link>
+        <Link className="nav" to={"/Bruce"}>
+          Bruce
+        </Link>
+      </div>
+
+      {/* <FaFacebookF onClick={fbookClick} className="icons"/>
             <FaInstagram onClick={instaClick} className="icons"/> */}
-            {/* <FaMapMarkerAlt onClick={mapClick} className="icons"/> */}
-            
+      {/* <FaMapMarkerAlt onClick={mapClick} className="icons"/> */}
     </div>
-    )
-}
+  );
+};
 
-export default footer
+export default footer;
 
 //FaFacebookSquare
 //FaInstagram
-
